3a1e1114a2602de172a0258b2bfb3dc249a446fe
Giovanni Beijl <531406@student.glu.nl>
2019-10-24 00:54:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,105 +111,105 @@ bot.on("message", message => {
     }
 })
 
-bot.on('message', async message => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
+// bot.on('message', async message => {
+// 	if (message.author.bot) return;
+// 	if (!message.content.startsWith(prefix)) return;
 
-	const serverQueue = queue.get(message.guild.id);
+// 	const serverQueue = queue.get(message.guild.id);
 
-	if (message.content.startsWith(`${prefix}play`)) {
-		execute(message, serverQueue);
-		return;
-	} else if (message.content.startsWith(`${prefix}skip`)) {
-		skip(message, serverQueue);
-		return;
-	} else if (message.content.startsWith(`${prefix}stop`)) {
-		stop(message, serverQueue);
-		return;
-	} else {
-		message.channel.send('Wat de kanker was dat')
-	}
-});
+// 	if (message.content.startsWith(`${prefix}play`)) {
+// 		execute(message, serverQueue);
+// 		return;
+// 	} else if (message.content.startsWith(`${prefix}skip`)) {
+// 		skip(message, serverQueue);
+// 		return;
+// 	} else if (message.content.startsWith(`${prefix}stop`)) {
+// 		stop(message, serverQueue);
+// 		return;
+// 	} else {
+// 		message.channel.send('Wat de kanker was dat')
+// 	}
+// });
 
-async function execute(message, serverQueue) {
-	const args = message.content.split(' ');
+// async function execute(message, serverQueue) {
+// 	const args = message.content.split(' ');
 
-	const voiceChannel = message.member.voiceChannel;
-	if (!voiceChannel) return message.channel.send('Je moet in een kanaal zitten!');
-	const permissions = voiceChannel.permissionsFor(message.client.user);
-	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-		return message.channel.send('Ik heb toestemming nodig om de vc te joinen!');
-	}
+// 	const voiceChannel = message.member.voiceChannel;
+// 	if (!voiceChannel) return message.channel.send('Je moet in een kanaal zitten!');
+// 	const permissions = voiceChannel.permissionsFor(message.client.user);
+// 	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+// 		return message.channel.send('Ik heb toestemming nodig om de vc te joinen!');
+// 	}
 
-	const songInfo = await ytdl.getInfo(args[1]);
-	const song = {
-		title: songInfo.title,
-		url: songInfo.video_url,
-	};
+// 	const songInfo = await ytdl.getInfo(args[1]);
+// 	const song = {
+// 		title: songInfo.title,
+// 		url: songInfo.video_url,
+// 	};
 
-	if (!serverQueue) {
-		const queueContruct = {
-			textChannel: message.channel,
-			voiceChannel: voiceChannel,
-			connection: null,
-			songs: [],
-			volume: 5,
-			playing: true,
-		};
+// 	if (!serverQueue) {
+// 		const queueContruct = {
+// 			textChannel: message.channel,
+// 			voiceChannel: voiceChannel,
+// 			connection: null,
+// 			songs: [],
+// 			volume: 5,
+// 			playing: true,
+// 		};
 
-		queue.set(message.guild.id, queueContruct);
+// 		queue.set(message.guild.id, queueContruct);
 
-		queueContruct.songs.push(song);
+// 		queueContruct.songs.push(song);
 
-		try {
-			var connection = await voiceChannel.join();
-			queueContruct.connection = connection;
-			play(message.guild, queueContruct.songs[0]);
-		} catch (err) {
-			console.log(err);
-			queue.delete(message.guild.id);
-			return message.channel.send(err);
-		}
-	} else {
-		serverQueue.songs.push(song);
-		console.log(serverQueue.songs);
-		return message.channel.send(`${song.title} is toegevoegd aan de wachtrij`);
-	}
+// 		try {
+// 			var connection = await voiceChannel.join();
+// 			queueContruct.connection = connection;
+// 			play(message.guild, queueContruct.songs[0]);
+// 		} catch (err) {
+// 			console.log(err);
+// 			queue.delete(message.guild.id);
+// 			return message.channel.send(err);
+// 		}
+// 	} else {
+// 		serverQueue.songs.push(song);
+// 		console.log(serverQueue.songs);
+// 		return message.channel.send(`${song.title} is toegevoegd aan de wachtrij`);
+// 	}
 
-}
+// }
 
-function skip(message, serverQueue) {
-	if (!message.member.voiceChannel) return message.channel.send('Je moet in een voice kanaal zitten om muziek te stoppen');
-	if (!serverQueue) return message.channel.send('Er is geen liedje dat ik kan skippen');
-	serverQueue.connection.dispatcher.end();
-}
+// function skip(message, serverQueue) {
+// 	if (!message.member.voiceChannel) return message.channel.send('Je moet in een voice kanaal zitten om muziek te stoppen');
+// 	if (!serverQueue) return message.channel.send('Er is geen liedje dat ik kan skippen');
+// 	serverQueue.connection.dispatcher.end();
+// }
 
-function stop(message, serverQueue) {
-	if (!message.member.voiceChannel) return message.channel.send('Je moet in een voice kanaal zitten om muziek te stoppen ')
-	serverQueue.songs = [];
-	serverQueue.connection.dispatcher.end();
-}
+// function stop(message, serverQueue) {
+// 	if (!message.member.voiceChannel) return message.channel.send('Je moet in een voice kanaal zitten om muziek te stoppen ')
+// 	serverQueue.songs = [];
+// 	serverQueue.connection.dispatcher.end();
+// }
 
-function play(guild, song) {
-	const serverQueue = queue.get(guild.id);
+// function play(guild, song) {
+// 	const serverQueue = queue.get(guild.id);
 
-	if (!song) {
-		serverQueue.voiceChannel.leave();
-		queue.delete(guild.id);
-		return;
-	}
+// 	if (!song) {
+// 		serverQueue.voiceChannel.leave();
+// 		queue.delete(guild.id);
+// 		return;
+// 	}
 
-	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-		.on('end', () => {
-			console.log('Music ended!');
-			serverQueue.songs.shift();
-			play(guild, serverQueue.songs[0]);
-		})
-		.on('error', error => {
-			console.error(error);
-		});
-	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-}
+// 	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+// 		.on('end', () => {
+// 			console.log('Music ended!');
+// 			serverQueue.songs.shift();
+// 			play(guild, serverQueue.songs[0]);
+// 		})
+// 		.on('error', error => {
+// 			console.error(error);
+// 		});
+// 	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+// }
 
 
 //starboard
