aa97681336cc81ce4a062d4809ca575d565fb402
Giovanni <531406@student.glu.nl>
2019-11-03 14:10:23 +0100

    koko

--- commands/unmute.js
+++ commands/unmute.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args)=>{
     if(!muterole) return message.channel.send(`Kanker op`)
 
     mutee.removeRole(muterole.id).then(()=>{
-        mutee.send(`Gegroet makker, je bent geunmute in ${message.guild.name} for ${reason}`).catch(err => console.log(err))
+        mutee.send(`Gegroet makker, je bent geunmute in ${message.guild.name} voor: ${reason}`).catch(err => console.log(err))
         message.channel.send(`${mutee.user.username} is geunmute`)
 
     })
