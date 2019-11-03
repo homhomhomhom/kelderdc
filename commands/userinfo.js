6ea2e8ee4aafa3c4ba12925a3d288bce2f54162c
Giovanni <531506@student.glu.nl>
2019-11-03 23:54:42 +0100

    hoi

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const moment = require('moment')
-
+let xp = require('../xp.json')
 module.exports.run = async(bot, message, args)=>{
     let user 
     if(message.mentions.users.first()){
@@ -11,6 +11,9 @@ module.exports.run = async(bot, message, args)=>{
 
     const member = message.guild.member(user)
 
+    let curxp = xp[message.author.id].xp;
+    let curlvl = xp[message.author.id].level;
+
     const embed = new Discord.RichEmbed()
         .setColor("RANDOM")
         .setThumbnail(message.author.avatarURL)
@@ -20,8 +23,8 @@ module.exports.run = async(bot, message, args)=>{
         .addField('Bot :', `${user.bot}`, true)
         .addField("Status :", `${user.presence.status}`, true)
         .addField("Game :", `${user.presence.game ? user.presence.game.name : "Geen game"}`, true)
+        .addField('Huidig level :', curlvl + ' ' + '('+curxp+ ' ' +'xp' +')')
         .addField("Rollen :", member.roles.map(roles => `${roles.name}`).join(', '),true)
-    
         message.channel.send({embed})
 }
 
