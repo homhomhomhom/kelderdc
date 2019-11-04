e98f0caa01f73fcfccf6c3740ed9e547be2fc191
Giovanni <531506@student.glu.nl>
2019-11-04 11:48:29 +0100

    hallo

--- commands/level.js
+++ commands/level.js
@@ -4,29 +4,39 @@ let purple = botconfig.purple;
 let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
-
-  if(!xp[message.author.id]){
-   xp[message.author.id] = {
-     xp: 0,
-     level: 1
-  };
-}
+  if (!xp[message.author.id]) {
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
   let curxp = xp[message.author.id].xp;
   let curlvl = xp[message.author.id].level;
   let nxtLvlXp = curlvl * 300;
   let difference = nxtLvlXp - curxp;
 
+  let user;
+  if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+  } else {
+    user = message.author;
+  }
+
   let lvlEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("RANDOM")
-  .addField("Level", curlvl, true)
-  .addField("XP", curxp, true)
-  .setFooter(`${difference} xp tot het volgend leveltje`, message.author.displayAvatarURL);
+    .setAuthor(user.username)
+    .setColor("RANDOM")
+    .addField("Level", curlvl, true)
+    .addField("XP", curxp, true)
+    .setFooter(
+      `${difference} xp tot het volgend leveltje`,
+      user.displayAvatarURL
+    );
 
-  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
-
-}
+  message.channel.send(lvlEmbed).then(msg => {
+    msg.delete(5000);
+  });
+};
 
 module.exports.help = {
   name: "level"
-}
+};
