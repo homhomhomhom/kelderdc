a436e78413099cf9710c5b186ec348b861114c7f
Giovanni <531506@student.glu.nl>
2019-11-08 11:37:31 +0100

    finished leaderboard and .level function

--- commands/level.js
+++ commands/level.js
@@ -1,21 +1,22 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(bot, message, args, con) =>{
-    con.query(`SELECT * FROM userLevels WHERE userID = ${message.author.id}`, (err, results)=>{
+    let user;
+    if(message.mentions.users.first()){
+        user = message.mentions.users.first()
+    }else{
+        user = message.author
+    }
+    con.query(`SELECT * FROM userLevels WHERE userID = ${user.id}`, (err, results)=>{
         if(err) throw err;
-        let user;
-        if(message.mentions.users.first()){
-            user = message.mentions.users.first()
-        }else{
-            user = message.author
-        }
+  
         let lvlembed = new Discord.RichEmbed()
             .setTitle('Level')
             .setAuthor(user.username)
             .setColor("RANDOM")
             .addField('Level', `${results[0].userLevel}`, true)
             .addField("XP", `${results[0].userXP}`, true)
-            .setFooter('uwu', user.displayAvtarURL)
+            .setFooter('uwu', user.displayAvatarURL)
 
         message.channel.send(lvlembed).then(message => {
             message.delete(5000)
