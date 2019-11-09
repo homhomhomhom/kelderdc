c1ecfc713f572b3ef38cdedb47992328d599b68c
Giovanni <531406@student.glu.nl>
2019-11-09 11:51:48 +0100

    update

--- commands/verificatie.js
+++ commands/verificatie.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const botconfig = require('../botconfig.json')
 
 module.exports.run = async(bot, message, args)=>{
+    if(message.author === message.b)
     let Leden = message.guild.roles.find(r => r.name === "Kelder Leden")
     const guildMember = message.member
     if(message.content === botconfig.prefix + "verifieer"){
